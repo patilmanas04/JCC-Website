3213ac31eecc6227a11ae480ccf26f6fd84ea2a2
patilmanas04 <patilmanas1304@gmail.com>
2024-10-05 00:35:46 +0530

    Do some changes

--- Client/src/components/Navbar/index.jsx
+++ Client/src/components/Navbar/index.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                     </NavbarLogo>
                     <NavbarMenu>
                         <NavbarItem>
-                            <NavbarLink href="#">About Us</NavbarLink>
+                            <NavbarLink href="#about">About Us</NavbarLink>
                         </NavbarItem>
                         <NavbarItem>
                             <NavbarLink href="#">Services</NavbarLink>
