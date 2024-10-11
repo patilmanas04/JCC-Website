351bf949bc0b7d6f319708e84ab625995aab9780
patilmanas04 <patilmanas1304@gmail.com>
2024-10-11 19:05:46 +0530

    Add theme colors and basic setup for development

---
+++ Client/src/utils/themes.js
@@ -0,0 +1,15 @@
+const lightTheme = {
+    primary: "#333333",
+    secondary: "#F5F5F5",
+    background: "#FFFFFF",
+    accent: "#FF5233",
+    secondaryText: "#AFAFAF",
+    secondaryAccent: "#F4835F",
+    divider: "#D9D9D9",
+}
+
+const darkTheme = {
+
+}
+
+export { lightTheme, darkTheme }
