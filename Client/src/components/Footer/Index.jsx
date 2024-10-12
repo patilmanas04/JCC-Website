c455268c9ad8f8b34bf3ae857376ad595dc8043a
patilmanas04 <patilmanas1304@gmail.com>
2024-10-13 02:42:01 +0530

    Add the Instagram link of the company and icon in the Footer

--- Client/src/components/Footer/Index.jsx
+++ Client/src/components/Footer/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LogoImage from '../../assets/logo.png'
 import EmailIcon from '../../assets/email_icon.svg'
 import PhoneIcon from '../../assets/phone_icon.svg'
+import InstagramIcon from '../../assets/instagram_icon.svg'
 import { FooterWrapper, FooterContainer, FooterInformation, CompanyName, Logo, CompanyNameText, CompanyAddress, AddressTitle, Address, CompanyContact, ContactDetails, ContactIcon, ContactText, CompanySocialMedia, SocialMediaTitle, SocialMediaLinks, SocialMediaLink, SocialMediaIcon, FooterLinks, LinksTitle, QuickLinks, QuickLink} from './FooterElements'
 
 const Footer = () => {
@@ -30,10 +31,7 @@ const Footer = () => {
                     <CompanySocialMedia>
                         <SocialMediaTitle>Follow Us:</SocialMediaTitle>
                         <SocialMediaLinks>
-                            <SocialMediaLink><SocialMediaIcon src=""/></SocialMediaLink>
-                            <SocialMediaLink><SocialMediaIcon src=""/></SocialMediaLink>
-                            <SocialMediaLink><SocialMediaIcon src=""/></SocialMediaLink>
-                            <SocialMediaLink><SocialMediaIcon src=""/></SocialMediaLink>
+                            <SocialMediaLink href='https://www.instagram.com/johari_counsultancy?igsh=Yzl5NnM0NWZ0bGhy' target='_blank'><SocialMediaIcon src={InstagramIcon}/></SocialMediaLink>
                         </SocialMediaLinks>
                     </CompanySocialMedia>
                 </FooterInformation>
