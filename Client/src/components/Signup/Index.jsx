c446d985a34225cff1115b22e4050d4fef62da39
patilmanas04 <patilmanas1304@gmail.com>
2024-10-13 02:06:10 +0530

    Complete the development of Sign up page and fix the issue where the login page overflows when it is get's out of the screenb

---
+++ Client/src/components/Signup/Index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { SignupWrapper, SignupContainer, SignupContent, SignupTitle, SignupSubtitle, SignupForm, SignupField, Label, Input, SignupButton, SignupFooter, Span } from './SignupElements'
+
+const Signup = () => {
+    return (
+        <SignupWrapper>
+            <SignupContainer>
+                <SignupContent>
+                    <SignupTitle>Create a new account👋</SignupTitle>
+                    <SignupSubtitle>It's quick and easy!
+                    </SignupSubtitle>
+                </SignupContent>
+                <SignupForm>
+                    <SignupField>
+                        <Label htmlFor='email'>Email</Label>
+                        <Input id='email' type="email" placeholder="Enter your email" />
+                    </SignupField>
+                    <SignupField>
+                        <Label htmlFor='password'>Password</Label>
+                        <Input id='password' name='password' type="password" placeholder="Password must be 8 characters long" />
+                    </SignupField>
+                    <SignupField>
+                        <Label htmlFor='confirmPassword'>Confirm Password</Label>
+                        <Input id='confirmPassword' name='confirmPassword' type="password" placeholder="Enter password again" />
+                    </SignupField>
+                    <SignupButton type='submit'>Sign up</SignupButton>
+                </SignupForm>
+                <SignupFooter>
+                    Already have an account? <Span>Login</Span>
+                </SignupFooter>
+            </SignupContainer>
+        </SignupWrapper>
+    )
+}
+
+export default Signup
