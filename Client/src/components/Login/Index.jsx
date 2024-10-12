41c5929c53a75abc32657ea787e2302ebe809cdc
patilmanas04 <patilmanas1304@gmail.com>
2024-10-13 00:38:50 +0530

    Complete the development of the UI of Login page but the functionality of the login page is pending

---
+++ Client/src/components/Login/Index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { LoginWrapper, LoginContainer, LoginContent, LoginTitle, LoginSubtitle, LoginForm, LoginField, Label, Input, LoginButton, LoginFooter, Span } from './LoginElements'
+
+const Login = () => {
+    return (
+        <LoginWrapper>
+            <LoginContainer>
+                <LoginContent>
+                    <LoginTitle>WELCOME BACK 👋</LoginTitle>
+                    <LoginSubtitle>Welcome back! Please enter your details.
+                    </LoginSubtitle>
+                </LoginContent>
+                <LoginForm>
+                    <LoginField>
+                        <Label htmlFor='email'>Email</Label>
+                        <Input id='email' type="email" placeholder="Enter your email" />
+                    </LoginField>
+                    <LoginField>
+                        <Label htmlFor='password'>Password</Label>
+                        <Input id='password' name='password' type="password" placeholder="Enter your password" />
+                    </LoginField>
+                    <LoginButton type='submit'>Log in</LoginButton>
+                </LoginForm>
+                <LoginFooter>
+                    Don't have an account? <Span>Sign up for free!</Span>
+                </LoginFooter>
+            </LoginContainer>
+        </LoginWrapper>
+    )
+}
+
+export default Login
