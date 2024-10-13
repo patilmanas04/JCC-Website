fb25845f3c72c81c0beccd50f317ef594c1cdbcf
patilmanas04 <patilmanas1304@gmail.com>
2024-10-13 14:31:52 +0530

    Basic setup of the express js backend server and add the sendEmail function in the utils

---
+++ Server/utils/sendEmail.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+
+const sendEmail = (to, subject, text) => {
+    try{
+        const transporter = nodemailer.createTransport({
+            service: process.env.MAIL_SERVICE,
+            port: process.env.MAIL_PORT,
+            secure: true,
+            auth: {
+                user: process.env.MAIL_AUTH_USER,
+                pass: process.env.MAIL_AUTH_PASS
+            }
+        })
+    
+        const mailOptions = {
+            from: process.env.MAIL_AUTH_USER,
+            to: to,
+            subject: subject,
+            text: text
+        }
+    
+        transporter.sendMail(mailOptions)
+
+        console.log("Email sent successfully")
+    }
+    catch(err){
+        console.log("Error sending email")
+        console.log(err)
+    }
+}
+
+module.exports = sendEmail
